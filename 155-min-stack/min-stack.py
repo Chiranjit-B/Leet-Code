5c61277ecec425d80029300a20176bce4ee30bfd
Chiranjit Banerjee <144847233+Chiranjit-B@users.noreply.github.com>
2026-01-08 09:43:47 -0500

    Time: 4 ms (69.64%) | Memory: 22.4 MB (5.66%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,8 +1,8 @@
 class MinStack:
 
     def __init__(self):
-        self.stack = [] # default stack for regular stack operations
-        self.min_stack = [] # default stack for special min operations
+        self.stack = []
+        self.min_stack = []
         
 
     def push(self, val: int) -> None:
@@ -10,8 +10,9 @@ class MinStack:
         if not self.min_stack :
             self.min_stack.append(val)
         else :
-            val = min(self.min_stack[-1], val)
-            self.min_stack.append(val)     
+            val = min(self.min_stack[-1],val)
+            self.min_stack.append(val)
+        
 
     def pop(self) -> None:
         self.stack.pop()
