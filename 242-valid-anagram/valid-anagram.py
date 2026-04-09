307378163e1016e5a9def2bf4a9336012819e26c
Chiranjit Banerjee <144847233+Chiranjit-B@users.noreply.github.com>
2026-04-09 18:33:04 -0400

    Time: 15 ms (43.14%) | Memory: 19.3 MB (82.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -4,12 +4,15 @@ class Solution:
         if len(s) != len(t) :
             return False
         else :
+                
             for i in range(len(s)) :
                 ds[s[i]] = 1 + ds.get(s[i],0)
-                dt[t[i]] = 1 + dt.get(t[i],0)
-
-            for i in ds :
+            for i in range(len(t)) :
+                dt[t[i]] = 1 + dt.get(t[i],0) 
+            
+            for i in s :
                 if ds[i] != dt.get(i,0) :
                     return False
-        return True 
+        return True
+
         
