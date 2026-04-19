83b3d3099702fd51eac3732cfdade3d3f4c87386
Chiranjit Banerjee <144847233+Chiranjit-B@users.noreply.github.com>
2026-04-19 16:59:20 -0400

    Add: is-anagram - submission-28

---
+++ Data Structures & Algorithms/is-anagram/submission-28.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t) :
+            return False
+        else :
+            ds = {}
+            dt = {}
+            for i in range(len(s)) :
+                ds[s[i]] = 1 + ds.get(s[i],0)
+
+            for i in range(len(t)):
+                dt[t[i]] = 1 + dt.get(t[i],0)
+            
+            for i in ds :
+                if ds[i] != dt.get(i,0) :
+                    return False
+            
+        return True
