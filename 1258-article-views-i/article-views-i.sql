021234334d9585aa9716bc47c21be252b02d574e
Chiranjit Banerjee <144847233+Chiranjit-B@users.noreply.github.com>
2026-06-06 14:01:13 -0400

    Time: 427 ms (53.77%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-SELECT DISTINCT(a.author_id) as id FROM Views a
-JOIN Views b 
-ON a.article_id = b.article_id
-WHERE a.author_id = b.viewer_id
+SELECT DISTINCT (author_id) AS id 
+FROM Views 
+WHERE author_id = viewer_id 
 ORDER BY id ASC
